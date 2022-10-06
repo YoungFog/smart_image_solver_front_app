8540c968423610a716868d445615d833bf239df1
YoungFog <youngfogcn@gmail.com>
2022-10-07 04:30:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+
+Vue.prototype.serverUrl = "http://smartimagesolver.youngfog.cn/";
+
+app.$mount()
